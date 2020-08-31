32e521e9749344c534efa999be7559e01a225427
Blue <13604413+Bluethefurry@users.noreply.github.com>
2020-08-31 10:06:21 +0200

    update loghandler

--- shared/useful/loghandler.lua
+++ shared/useful/loghandler.lua
@@ -1,7 +1,6 @@
-Citizen.CreateThread(function()
-	function writeLog(msg,level)
-		if level <= config.LogLevel then 
-			print(msg)
-		end
+
+function writeLog(msg,level)
+	if level <= config.LogLevel then 
+		print(msg)
 	end
-end)
+end
