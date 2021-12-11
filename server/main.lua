bdc2a1baeb12499cc3d240945a52439d8d45d479
Robi321 <46158539+Robi321@users.noreply.github.com>
2021-12-11 08:42:58 +0100

    VERSION Update 1.0

--- server/main.lua
+++ server/main.lua
@@ -72,6 +72,27 @@ function stringsplit(inputstr, sep)
 	return t
 end
 
+local verFile = LoadResourceFile(GetCurrentResourceName(), "version.json")
+local curVersion = json.decode(verFile).version
+Citizen.CreateThread( function()
+	local updatePath = "/Blumlaut/RottenV"
+	local resourceName = "AntiCheese ("..GetCurrentResourceName()..")"
+	PerformHttpRequest("https://raw.githubusercontent.com"..updatePath.."/master/version.json", function(err, response, headers)
+		local data = json.decode(response)
+		
+		
+		if curVersion ~= data.version and tonumber(curVersion) < tonumber(data.version) then
+			print("\n--------------------------------------------------------------------------")
+			print("\n"..resourceName.." is outdated.\nCurrent Version: "..data.version.."\nYour Version: "..curVersion.."\nPlease update it from https://github.com"..updatePath.."")
+			print("\nUpdate Changelog:\n"..data.changelog)
+			print("\n--------------------------------------------------------------------------")
+		elseif tonumber(curVersion) > tonumber(data.version) then
+			print("Your version of "..resourceName.." seems to be higher than the current version.")
+		else
+			print(resourceName.." is up to date!")
+		end
+	end, "GET", "", {version = 'this'})
+end)
 
 -- removed for public release.
 --[[
